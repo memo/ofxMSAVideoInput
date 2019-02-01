ca7bd4d078a85d60085ab6ca754df016ee9d3457
Memo Akten <memo@memo.tv>
2019-02-01 04:52:51 +0300

    undef libdc

--- src/MSAVideoInput.h
+++ src/MSAVideoInput.h
@@ -9,7 +9,7 @@
 
 #pragma once
 
-#define USE_OFXLIBDC
+//#define USE_OFXLIBDC
 
 #include "ofMain.h"
 #include "MSACore.h"
@@ -20,28 +20,28 @@
 #include "ofxSimpleGuiToo.h"
 
 namespace msa {
-    
+
 	class VideoInput {//: public ofBaseVideo {
 	public:
 		VideoInput();
 		~VideoInput();
-        
+
         enum {
             kVideoGrabber,
             kVideoPlayer,
             kofxLibdc,
         } inputType;
-        
+
 		struct {
 			ofBaseVideo			*current;				// points to one of below
-            
+
 			ofVideoPlayer       *player;
 			ofVideoGrabber		*ofGrabber;
 #ifdef USE_OFXLIBDC
             ofxLibdc::Grabber    *libdcGrabber;
 #endif
 		} inputDevice;
-        
+
 		bool	enabled;
 		bool	doDraw;
 		bool	doDrawGrid;
@@ -55,7 +55,7 @@ namespace msa {
 		bool	captureColor;					// whether we should try to capture Y8 or RGB
 		bool	restart;
 		bool	showSettings;
-        
+
 		struct {
 			bool	togglePlay;
 			bool	reset;
@@ -64,22 +64,22 @@ namespace msa {
 			float	position;
 			int		frame;
 		} videoControls;
-        
+
 		struct {
 			Vec2f	size;
 			Vec2f	invSize;
-            
+
 			float	aspectRatio;
 			float	invAspectRatio;
-            
+
 			bool	inputIsColor;			// whether capture is Y8 or RGB
 			bool	hasNewFrame;
-			
+
 			float	captureFPS;
 			float	lastCaptureTime;
-			
+
 		} info;
-        
+
         struct {
             float brightness;
             float gamma;
@@ -87,44 +87,43 @@ namespace msa {
             float shutter;
             float gain;
         } libdcSettings, oldLibdcSettings;   // FIX: crappy way of doing it
-        
-        
+
+
 		void reset();
 		void setup();
 		void update();
 		void draw();
-        
+
 		void drawUI();
-        
+
 		void loadMovie(string s);
-		
+
 		void setInputVideofilename(string s);
-        
+
 		void clear();
 		void initInput();
-        
+
 		bool isVideo();
 		bool isOfGrabber();
 		bool isOfxGrabber();
-        
+
         bool isReady();
 		unsigned char *getPixelsData() const;
         ofPixelsRef getPixels() const;
 		float getWidth() const;
 		float getHeight() const;
-        
+
 		void draw(float x, float y, float w, float h);
-        
-        
+
+
         bool isFrameNew() {
             return inputDevice.current && info.hasNewFrame;
         }
-        
+
 	protected:
 		void			setupUI();
 //        void            keyPressed(ofKeyEventArgs &e);
 		string			inputVideoFilename;
 	};
-    
-}
 
+}
