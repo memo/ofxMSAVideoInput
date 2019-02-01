48bab5806faf6e89f84d3c0a1dbacbe0f750d630
Memo Akten <memo@memo.tv>
2019-02-01 04:52:22 +0300

    addon_config

---
+++ addon_config.mk
@@ -0,0 +1,11 @@
+meta:
+	ADDON_NAME = ofxMSAVideoInput
+	ADDON_DESCRIPTION =
+	ADDON_AUTHOR = Memo Akten, www.memo.tv
+	ADDON_TAGS = ""
+	ADDON_URL = https://github.com/memo/ofxMSAVideoInput
+
+common:
+	# dependencies with other addons, a list of them separated by spaces
+	# or use += in several lines
+	ADDON_DEPENDENCIES = ofxMSACore ofxSimpleGuiToo
